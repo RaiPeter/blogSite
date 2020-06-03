e56c7aec1ca8733ca76a79f8c58e37a929612c09
RaiPeter <rai.peter2@gmail.com>
2020-06-04 00:45:55 +0530

    Updates

---
+++ precache-manifest.5299cf61d865cabb0907394639f40caa.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cd05eca840e26806355f48a16a88ad2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "081e732ffa8f4f10a7d6",
+    "url": "/static/css/main.a969ff5a.chunk.css"
+  },
+  {
+    "revision": "f99c4a60a7001bca114f",
+    "url": "/static/js/2.cbcf4646.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cbcf4646.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "081e732ffa8f4f10a7d6",
+    "url": "/static/js/main.f9a978ba.chunk.js"
+  },
+  {
+    "revision": "b6ebbf9d3a57d0f1ef9d",
+    "url": "/static/js/runtime-main.5eeb4e9f.js"
+  },
+  {
+    "revision": "80961a00ad954f1df9a3356d8e386900",
+    "url": "/static/media/affection-baby-baby-girl-beautiful-377058.80961a00.jpg"
+  },
+  {
+    "revision": "4218ad9257e6d8b09e88ad06c8308e04",
+    "url": "/static/media/beautiful-&-simple.4218ad92.jpg"
+  },
+  {
+    "revision": "a67df1389674b05ae6d1b50ad59a30b1",
+    "url": "/static/media/cestovat-chladny-dno-jednoduchost-2868847.a67df138.jpg"
+  },
+  {
+    "revision": "45c60c8a7cce7c53f5988fcdb2b71656",
+    "url": "/static/media/fitness-blog-post.45c60c8a.jpg"
+  },
+  {
+    "revision": "13ad316bf606c371e9f32ad600e9f949",
+    "url": "/static/media/memories-from.13ad316b.jpg"
+  }
+]);
