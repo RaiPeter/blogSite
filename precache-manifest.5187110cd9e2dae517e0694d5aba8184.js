e0afb4f9870111d8d3cbee30050e3d016c5e8c02
RaiPeter <rai.peter2@gmail.com>
2020-06-04 00:48:23 +0530

    Updates

---
+++ precache-manifest.5187110cd9e2dae517e0694d5aba8184.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c17011cac921ef4f655ee7f824ca1eb1",
+    "url": "/blogSite/index.html"
+  },
+  {
+    "revision": "99a219fdf22ccbe44507",
+    "url": "/blogSite/static/css/main.a969ff5a.chunk.css"
+  },
+  {
+    "revision": "f99c4a60a7001bca114f",
+    "url": "/blogSite/static/js/2.cbcf4646.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/blogSite/static/js/2.cbcf4646.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99a219fdf22ccbe44507",
+    "url": "/blogSite/static/js/main.d5837263.chunk.js"
+  },
+  {
+    "revision": "7d053ee37b1ee424cb7d",
+    "url": "/blogSite/static/js/runtime-main.ba849216.js"
+  },
+  {
+    "revision": "80961a00ad954f1df9a3356d8e386900",
+    "url": "/blogSite/static/media/affection-baby-baby-girl-beautiful-377058.80961a00.jpg"
+  },
+  {
+    "revision": "4218ad9257e6d8b09e88ad06c8308e04",
+    "url": "/blogSite/static/media/beautiful-&-simple.4218ad92.jpg"
+  },
+  {
+    "revision": "a67df1389674b05ae6d1b50ad59a30b1",
+    "url": "/blogSite/static/media/cestovat-chladny-dno-jednoduchost-2868847.a67df138.jpg"
+  },
+  {
+    "revision": "45c60c8a7cce7c53f5988fcdb2b71656",
+    "url": "/blogSite/static/media/fitness-blog-post.45c60c8a.jpg"
+  },
+  {
+    "revision": "13ad316bf606c371e9f32ad600e9f949",
+    "url": "/blogSite/static/media/memories-from.13ad316b.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5299cf61d865cabb0907394639f40caa.js"
+  "/blogSite/precache-manifest.5187110cd9e2dae517e0694d5aba8184.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/blogSite/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
